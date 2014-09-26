0637ac7b35fde52b8a244c68ee7b5440e5776617
Jack Peterson <jack@tinybike.net>
2014-09-25 17:45:16 -0700

    Some more test cases

--- src/WeightedStats.jl
+++ src/WeightedStats.jl
@@ -6,15 +6,15 @@ NumericArray = Union(Array{Int,1}, Array{Float32,1}, Array{Float64,1})
 
 function weighted_mean(data::NumericArray, weights::NumericArray)
     data = float(data)
-    weights = float(weights)
-    (data' * (weights ./ sum(weights)))[1]
+    weights = float(weights) / sum(weights)
+    (data' * weights)[1]
 end
 
 function weighted_median(data::NumericArray, weights::NumericArray)
     sorted = sortrows([data weights])
     midpoint = 0.5 * sum(sorted[:,2])
     if any(weights .> midpoint)
-        median(data[weights .== maximum(weights)])
+        (data[weights .== maximum(weights)])[1]
     else
         cumulative_weight = cumsum(sorted[:,2])
         below_midpoint_index = find(cumulative_weight .<= midpoint)[end]
