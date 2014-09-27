034b0af92552c04f86cb633290c34c1ff3bc95a9
Jack Peterson <jack@tinybike.net>
2014-09-26 18:55:32 -0700

    improve memory efficiency about 20 percent, add tests

--- src/WeightedStats.jl
+++ src/WeightedStats.jl
@@ -2,24 +2,35 @@ module WeightedStats
 
 export weighted_mean, weighted_median
 
-function weighted_mean{S<:Real,T<:Real}(data::Array{S,1}, weights::Array{T,1})
+function weighted_mean{T<:Real,W<:Real}(data::Array{T,1}, weights::Array{W,1})
     data = float(data)
     weights = float(weights) / sum(weights)
     (data' * weights)[1]
 end
 
-function weighted_median{S<:Real,T<:Real}(data::Array{S,1}, weights::Array{T,1})
-    sorted = sortrows([data weights])
-    midpoint = 0.5 * sum(sorted[:,2])
-    if any(weights .> midpoint)
-        (data[weights .== maximum(weights)])[1]
-    else
-        cumulative_weight = cumsum(sorted[:,2])
-        below_midpoint_index = find(cumulative_weight .<= midpoint)[end]
-        if cumulative_weight[below_midpoint_index] == midpoint
-            mean(sorted[below_midpoint_index:below_midpoint_index+1,1])
+function weighted_median{T<:Real,W<:Real}(data::Array{T,1}, weights::Array{W,1})
+    mask = weights .> 0
+    if any(mask)
+        data = data[mask]
+        weights = weights[mask]
+        sorted = sortrows([data weights])
+        midpoint = 0.5 * sum(weights)
+        if any(weights .> midpoint)
+            (data[weights .== maximum(weights)])[1]
         else
-            sorted[below_midpoint_index+1,1]
+            cumulative_weight = 0
+            below_midpoint_index = 0
+            while cumulative_weight <= midpoint
+                below_midpoint_index += 1
+                cumulative_weight += sorted[below_midpoint_index,2]
+            end
+            cumulative_weight -= sorted[below_midpoint_index,2]
+            below_midpoint_index -= 1
+            if cumulative_weight == midpoint
+                mean(sorted[below_midpoint_index:below_midpoint_index+1,1])
+            else
+                sorted[below_midpoint_index+1,1]
+            end
         end
     end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,6 @@
 using WeightedStats
 using Base.Test
 
-function approx_eq(a, b, tol)
-    abs(a - b) < tol
-end
-
-tol = 1e-6
-
 data = (
     [7, 1, 2, 4, 10],
     [7, 1, 2, 4, 10],
@@ -19,6 +13,11 @@ data = (
     [2, 0.6, 1.3, 0.3, 0.3, 1.7, 0.7, 1.7, 0.4],
     [3.7, 3.3, 3.5, 2.8],
     [100, 125, 123, 60, 45, 56, 66],
+    [2, 2, 2, 2, 2, 2],
+    [2.3],
+    [-2, -3, 1, 2, -10],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
 )
 weights = (
     [1, 1/3, 1/3, 1/3, 1],
@@ -32,12 +31,24 @@ weights = (
     [2, 2, 0, 1, 2, 2, 1, 6, 0],
     [5, 5, 4, 1],
     [30, 56, 144, 24, 55, 43, 67],
+    [0.1, 0.2, 0.3, 0.4, 0.5, 0.6],
+    [12],
+    [7, 1, 1, 1, 6],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 2],
 )
-median_answers = (7, 4, 8.5, 8.5, 10, 2.5, 5, 50, 1.7, 3.5, 100)
-mean_answers = (6.444444, 4.800000, 8.583333,
-                8.583333, 9.086956, 2.909091,
-                4.949617, 47.333333, 1.275,
-                3.453333, 91.782816)
+median_answers = (7.0,   4.0,  8.5,
+                  8.5,  10.0,  2.5,
+                  5.0,  50.0,  1.7,
+                  3.5, 100.0,  2.0,
+                  2.3,  -2.0,  3.0,
+                  5.0)
+mean_answers = (6.444444,  4.800000, 8.583333,
+                8.583333,  9.086956, 2.909091,
+                4.949617, 47.333333, 1.275000,
+                3.453333, 91.782816, 2.000000,
+                2.300000, -4.625000, 3.000000,
+                3.666667)
 
 num_tests = length(data)
 
@@ -47,6 +58,12 @@ num_tests = length(data)
 @test_throws MethodError weighted_mean("string", "input")
 
 for i = 1:num_tests
-    @test approx_eq(weighted_median(data[i], weights[i]), median_answers[i], tol)
-    @test approx_eq(weighted_mean(data[i], weights[i]), mean_answers[i], tol)
+    @test weighted_median(data[i], weights[i]) == median_answers[i]
+    @test_approx_eq_eps weighted_mean(data[i], weights[i]) mean_answers[i] 1e-6
 end
+
+datum = [4, 3, 2, 1]
+weight = [0, 0, 0, 0]
+
+@test is(weighted_median(datum, weight), nothing)
+@test isnan(weighted_mean(datum, weight))
