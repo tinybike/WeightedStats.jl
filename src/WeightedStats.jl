6a6b5953c44d12d85534a1b39cdd95a12ac724ab
Jack Peterson <jack@tinybike.net>
2014-09-29 01:12:44 -0700

    Using sortperm instead of sortrows for improved speed, memory use

--- src/WeightedStats.jl
+++ src/WeightedStats.jl
@@ -8,25 +8,27 @@ function weighted_mean{T<:Real,W<:Real}(data::Array{T,1}, weights::Array{W,1})
 end
 
 function weighted_median{T<:Real,W<:Real}(data::Array{T,1}, weights::Array{W,1})
-    mask = (weights .> 0)::BitArray{1}
+    mask = weights .> 0
     if any(mask)
         data = data[mask]
         weights = weights[mask]
-        sorted = sortrows([data weights])
         midpoint = 0.5 * sum(weights)
         if any(weights .> midpoint)
             first(data[weights .== maximum(weights)])
         else
-            cumulative_weight = below_midpoint_index = 0
-            while cumulative_weight <= midpoint
-                below_midpoint_index += 1
-                cumulative_weight += sorted[below_midpoint_index,2]
+            permute = sortperm(data)
+            i = cumulative_weight = 0
+            for (i, p) in enumerate(permute)
+                if cumulative_weight > midpoint
+                    cumulative_weight -= weights[p]
+                    break
+                end
+                cumulative_weight += weights[p]
             end
-            cumulative_weight -= sorted[below_midpoint_index,2]
             if cumulative_weight == midpoint
-                mean(sorted[below_midpoint_index-1:below_midpoint_index,1])
+                0.5 * (data[permute[i-2]] + data[permute[i-1]])
             else
-                sorted[below_midpoint_index,1]
+                data[permute[i-1]]
             end
         end
     end
